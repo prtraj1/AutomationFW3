406576005880a622ed0e2520e388b001c5a4394a
Prithviraj Sahoo <prtraj009@gmail.com>
2022-12-28 14:55:53 +0530

    Updated TestBase

--- src/main/java/dummy/DummyClass.java
+++ src/main/java/dummy/DummyClass.java
@@ -48,10 +48,46 @@ public class DummyClass {
 //        System.out.println(exc2.getRowNumbersInColumnHavingText("pwd1234", 2));
 //        System.out.println(exc2.getColNumbersInRowHavingText("Username", 5));
 
-        String[][] data = new String[10][1];
-        data[0][0] = "something";
-        data[0][1] = "new";
+//        String[][] data = new String[10][1];
+//        data[0][0] = "something";
+//        data[0][1] = "new";
+//
+//        List<Integer> nums = Arrays.asList(2,3);
 
-        List<Integer> nums = Arrays.asList(2,3);
+        //Declaring an array (type 01)
+        int[] numbers = new int[]{90,89,111,110}; //1 Row and 4 columns --> linear array
+        int[][] twoDnumbers = new int[][]{
+                {90,89,111,110},
+                {90,89,101,711}
+        }; //multiple rows and columns --> 2D array
+
+        //Declaring an array (type 02)
+        int[] n1 = new int[3];
+        n1[1] = 90;
+        n1[2] = 89;
+        for(int i=0; i<n1.length; i++){
+            System.out.println(n1[i]);
+        }
+
+        String[] s1 = new String[3];
+        s1[1] = "anything";
+        s1[2] = "demo";
+        for(int i=0; i<n1.length; i++){
+            System.out.println(s1[i]);
+        }
+
+        int[][] n2 = new int[3][2];
+        n2[0][1] = 10;
+        n2[2][1] = 22;
+        System.out.println(n2.length);
+
+        for (int i=0; i<n2.length; i++){
+            for(int j = 0; j< n2[i].length; j++){
+                System.out.println(n2[i][j]);
+            }
+        }
+
+
+
     }
 }
